21a1eb56a87a392f3f743691812d2d86b3050e45
DESKTOP-MDAH4MF\ncg <tyu4778@gmail.com>
2023-11-13 16:43:01 +0900

    Update: Longest Consecutive Sequence 코드 수정
    - dictioary대신 set()사용

--- study/codingTest_[All In One]/3. Hash Table/Coding Test Example/2. Longest Consecutive Sequence1.py
+++ study/codingTest_[All In One]/3. Hash Table/Coding Test Example/2. Longest Consecutive Sequence1.py
@@ -1,28 +1,23 @@
-## O(n) : Dictionary
+## O(n) : newNumsionary
 
 class Solution(object):
     def longestConsecutive(self, nums):
-        memo = {}
+        newNums = set(nums)
         maxCnt = 0
 
-        ## O(n)
-        for num in nums:
-            memo[num] = 1
+        # dict = {}
+        # for num in nums:
+        #     dict[num] = 1
 
-        ## O(n)
-        for num in nums:
-            cnt = 1
-            target = num + 1
-            if num - 1 not in memo:
-                # 최대 n번만
-                while target in memo: # [in] Dictionary : O(1)
+        for num in newNums:
+            if num - 1 not in newNums:
+                cnt = 1
+                target = num + 1
+                while target in newNums:
                     cnt += 1
                     target += 1
-            cnt = max(cnt, maxCnt)
-            if maxCnt < cnt:
-                maxCnt = cnt
+                maxCnt = max(cnt, maxCnt)
         return maxCnt
-
 
 sol = Solution()
 answer = sol.longestConsecutive(nums = [100, 4, 200, 1, 3, 2])
