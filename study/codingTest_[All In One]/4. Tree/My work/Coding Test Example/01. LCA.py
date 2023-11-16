18ce13da53098ce468e474a24859a5e84fd8dab6
changon <tyu4778@gmail.com>
2023-11-16 22:37:44 +0900

    Update: LCA.py to debug

--- study/codingTest_[All In One]/4. Tree/My work/Coding Test Example/01. LCA.py
+++ study/codingTest_[All In One]/4. Tree/My work/Coding Test Example/01. LCA.py
@@ -1,11 +1,11 @@
 '''
 [ Lowest Common Ancestor of a Binary Tree ]
 Binary 트리 하나와 해당 트리에 속한 두 개의 노드가 주어질 때,
-두 노드의 공통 조상중 가장 낮은 node 즉, the lowest common ancestor(LCA)를 찾아라
+두 노드의 공통 조상중 가장 낮은 TreeNode 즉, the lowest common ancestor(LCA)를 찾아라
 '''
 
 
-# Definition for a binary tree node.
+# Definition for a binary tree TreeNode.
 class TreeNode(object):
     def __init__(self, x):
         self.val = x
@@ -19,9 +19,29 @@ class Solution(object):
         
         left = self.lowestCommonAncestor(root.left, p, q) 
         right = self.lowestCommonAncestor(root.right, p, q)
-        if root == p or root == q:
-            return root
+        if root.val == p or root.val == q:
+            return root.val
         elif left and right:
-            return root
+            return root.val
         return left or right
         
+
+bt = TreeNode(3)
+bt.left = TreeNode(5)
+bt.right = TreeNode(1)
+bt.left.left = TreeNode(6)
+bt.left.right = TreeNode(2)
+bt.left.right.left = TreeNode(7)
+bt.left.right.right = TreeNode(4)
+bt.right.left = TreeNode(0)
+bt.right.right = TreeNode(8)
+
+sol = Solution()
+answer = sol.lowestCommonAncestor(bt, 6, 4) # 5
+print(answer)
+answer = sol.lowestCommonAncestor(bt, 5, 1) # 3
+print(answer)
+
+
+
+
