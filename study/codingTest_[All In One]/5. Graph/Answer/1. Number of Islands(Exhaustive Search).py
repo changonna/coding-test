a38a9e86e709ae8b54b96ade7b5809f509b4fa0f
DESKTOP-MDAH4MF\ncg <tyu4778@gmail.com>
2023-11-23 17:37:45 +0900

    Create: 그래프-섬의개수 문제 내 풀이와 정답 구분

---
+++ study/codingTest_[All In One]/5. Graph/Answer/1. Number of Islands(Exhaustive Search).py
@@ -0,0 +1,64 @@
+# Graph
+# 완전탐색
+
+from collections import deque
+class Solution(object):
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        number_of_islands = 0
+        row = len(grid) # 4
+        col = len(grid[0]) # 5
+        visited = [[False] * col for _ in range(row)]
+
+        def bfs(x, y):
+           dx = [-1, 1, 0, 0]
+           dy = [0, 0, -1, 1]
+
+           visited[x][y] = True
+           queue = deque()
+           queue.append((x, y))
+           while queue:
+              cur_x, cur_y = queue.popleft()
+              for i in range(len(dx)):
+                next_x = cur_x + dx[i]
+                next_y = cur_y + dy[i]
+
+                # next_x, next_y 범위 안넘게 / 
+                if next_x >= 0 and next_x < row and next_y >= 0 and next_y < col:
+                  if grid[next_x][next_y] == '1' and not visited[next_x][next_y]:
+                    visited[next_x][next_y] = True
+                    queue.append((next_x, next_y))
+
+        # 완전탐색
+        for i in range(row):
+          for j in range(col):
+            # 값이 1이고 방문하지 않았으면
+            if grid[i][j] == '1' and not visited[i][j]:
+              bfs(i, j)
+              number_of_islands += 1
+
+        return number_of_islands
+
+
+   
+   
+sol = Solution()
+
+grid1 = [
+  ['1', '1', '1', '1', '0'],
+  ['1', '1', '0', '1', '0'],
+  ['1', '1', '0', '0', '0'],
+  ['0', '0', '0', '0', '0']
+]
+
+grid2 = [
+  ['1', '1', '1', '0', '0'],
+  ['1', '1', '0', '0', '0'],
+  ['1', '1', '0', '1', '0'],
+  ['0', '0', '0', '0', '1']
+]
+print(sol.numIslands(grid1)) # 1
+print(sol.numIslands(grid2)) # 3
--- study/codingTest_[All In One]/5. Graph/Coding Test Example/1. Number of Islands(Exhaustive Search).py
+++ study/codingTest_[All In One]/5. Graph/Coding Test Example/1. Number of Islands(Exhaustive Search).py
@@ -1,31 +1,80 @@
-# Graph
-# 완전탐색
+# from collections import deque
 
-from collections import deque
-class Solution(object):
+# graph = {
+#   'A': ['B'],
+#   'B': ['A', 'C', 'E'],
+#   'C': ['B', 'D'],
+#   'D': ['C', 'E', 'F'],
+#   'E': ['B', 'D', 'F'],
+#   'F': ['D', 'E']
+# }
+
+# def graphBfs(start_v):
+#     visited = [start_v]
+#     queue = deque(start_v)
+
+#     while queue:
+#         cur_v = queue.popleft()
+#         for v in graph[cur_v]:
+#             if v not in visited:
+#                 queue.append(v)
+#                 visited.append(v)
+
+#     return visited
+    
+# print(graphBfs('A'))
+
+
+
+####
+
+class Solution():
     def numIslands(self, grid):
-        """
-        :type grid: List[List[str]]
-        :rtype: int
-        """
-        cnt = 0
-        start_v = (0, 0)
-        visited = [start_v]
-        queue = deque(start_v)
+        number_of_islands = 0
+        # row, col 설정
+        row = len(grid)
+        col = len(grid[0])
+        # visited 설정
+        visited = [[False] * col for _ in range(row)]
 
-        for i in len(grid):
-            for j in len(grid[i]):
-              print('a')    
-            
-        
-        return cnt
+        def bfs(x, y):
+            # 상,하,좌,우 설정
+            mx = [-1, 1, 0, 0]
+            my = [0, 0, -1, 1]
 
-grid = [
+            for i in range(len(mx)):
+                # 현재 좌표에서 상,하,좌,우 좌표 구하기
+                next_x = x + mx[i]
+                next_y = y + my[i]
+
+                # 상,하,좌,우 테두리 설정
+                if next_x >= 0 and next_x < row and next_y >= 0 and next_y < col:
+                    # 땅(1)이고 방문하지 않은
+                    if grid[next_x][next_y] == '1' and not visited[next_x][next_y]:
+                        # visited에 해당 좌표 True로 변환
+                        visited[next_x][next_y] = True
+
+        # 완전탐색
+        for i in range(row):
+            for j in range(col):
+                # 땅(1)이고 방문하지 않은 
+                if grid[i][j] == '1' and not visited[i][j]:
+                    bfs(i, j)
+                    number_of_islands += 1
+        return number_of_islands
+
+
+
+
+
+
+sol = Solution()
+
+grid1 = [
   ['1', '1', '1', '1', '0'],
   ['1', '1', '0', '1', '0'],
   ['1', '1', '0', '0', '0'],
   ['0', '0', '0', '0', '0']
 ]
 
-sol = Solution()
-print(sol.numIslands(grid)) # 1
+print(sol.numIslands(grid1))
