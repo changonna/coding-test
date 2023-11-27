87641cb9f60dd89c6f68627107e95c9d3292ac86
DESKTOP-MDAH4MF\ncg <tyu4778@gmail.com>
2023-11-27 17:01:36 +0900

    Update: Key and Rooms(DFS) 개선한 코드 풀이 수정

--- study/codingTest_[All In One]/5. Graph/Coding Test Example/3. keyAndRooms(DFS)2.py
+++ study/codingTest_[All In One]/5. Graph/Coding Test Example/3. keyAndRooms(DFS)2.py
@@ -1,20 +1,19 @@
 # 개선한 코드
-# 1. visited를 List가 아닌 set()으로 변경하여 append ->
+# 1. visited를 List가 아닌 set()으로 변경하여 append -> add
 # 2. return시 if 조건문 제거
 
 from collections import deque
 
 class Solution(object):
-    
     def canVisitAllRooms(self, rooms):
-        visited = set()
-        def dfs(cur_v):
-            visited.add(cur_v)
-            for v in rooms[cur_v]:
+        visited = set() # set()
+        def dfs(roomNumber):
+            visited.add(roomNumber) # add
+            for v in rooms[roomNumber]:
                 if v not in visited:
-                    dfs(v)    
+                    dfs(v)
         dfs(0)
-        return len(visited) == len(rooms)
+        return len(visited) == len(rooms) # 조건문 삭제
 
 sol = Solution()
 rooms1 = [[1,3],[3,0,1],[2],[0]]
