988f715132278b032ab86972304dd84129cf0f0f
changon <tyu4778@gmail.com>
2023-11-29 21:45:07 +0900

    Update: fibonacci(bottom-up) 초기값 수정

--- study/codingTest_[All In One]/6. DP/My work/fibonacci(Bottom-up).py
+++ study/codingTest_[All In One]/6. DP/My work/fibonacci(Bottom-up).py
@@ -1,6 +1,12 @@
-memo = {}
+memo = {
+    1: 1,
+    2: 1
+}
 
 def fibo(n):
     for i in range(3, n + 1):
         memo[i] = memo[i - 1] + memo[i - 2]
     return memo[n]
+
+answer = fibo(4)
+print(answer)
