f41a4be5139c52cdc5c08658a1f980a6e5f855ab
changon <tyu4778@gmail.com>
2023-12-03 23:08:12 +0900

    Update: DP 문제 min cost climbing staris(Top-down)

--- study/codingTest_[All In One]/6. DP/My work/CodingTestExample/minCostClimbingStairs(Top-down).py
+++ study/codingTest_[All In One]/6. DP/My work/CodingTestExample/minCostClimbingStairs(Top-down).py
@@ -1,20 +1,24 @@
 # Top-down
 class Solution:
-    def uniquePaths(self, m, n):
-        memo = [[-1] * n for _ in range(m)]
+    def minCostClimbingStairs(self, cost):
+        memo = {}
+        n = len(cost)
 
-        def dp(r, c):
-            if r == 0 and c == 0:
-                memo[r][c] = 1
-                return memo[r][c]
+        def dp(n):
+            if n == 0 or n == 1:
+                return 0
+            if n not in memo:
+                memo[n] = min(dp(n-2) + cost[n-2], dp(n-1) + cost[n-1])
+            return memo[n]
+        return dp(n)
+        
+        
+sol = Solution()
 
-            if memo[r][c] == -1:
-                unique_paths = 0
-                if (r - 1) >= 0:
-                    unique_paths += dp(r - 1, c)
-                if (c - 1) >= 0:
-                    unique_paths += dp(r, c - 1)
-                memo[r][c] = unique_paths
-            return memo[r][c]
+cost1 = [10, 15, 20]
+result1 = sol.minCostClimbingStairs(cost1)
+print(result1) # 15
 
-        return dp(m - 1, n - 1)
+cost2 = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1]
+result2 = sol.minCostClimbingStairs(cost2)
+print(result2) # 6
