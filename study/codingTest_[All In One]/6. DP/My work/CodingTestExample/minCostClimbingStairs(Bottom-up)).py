5d3c31c42ff5a742f633e56b414b9b9aac7cf92f
changon <tyu4778@gmail.com>
2023-12-04 22:59:55 +0900

    Update: DP 문제 min cost climbing staris(Bottom-up)

--- study/codingTest_[All In One]/6. DP/My work/CodingTestExample/minCostClimbingStairs(Bottom-up)).py
+++ study/codingTest_[All In One]/6. DP/My work/CodingTestExample/minCostClimbingStairs(Bottom-up)).py
@@ -2,11 +2,23 @@
 class Solution(object):
     def minCostClimbingStairs(self, cost):
         n = len(cost)
-        dp = [-1] * (n + 1)
 
+        # list 초기화해서 영역 설정하기
+        dp = [-1] * (n) 
         dp[0] = 0
         dp[1] = 0
-        for i in range(2, n + 1):
-            dp[i] = min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2])
+        
+        for i in range(2, n+1):
+            dp[i] = min(dp[i-1] + cost[i-1], dp[i-2] + cost[i-2])
 
         return dp[n]
+
+sol = Solution()
+
+cost1 = [10, 15, 20]
+result1 = sol.minCostClimbingStairs(cost1)
+print(result1) # 15
+
+cost2 = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1]
+result2 = sol.minCostClimbingStairs(cost2)
+print(result2) # 6
