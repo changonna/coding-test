9d2a330ab415beecc80bdd480008321732e9d456
DESKTOP-MDAH4MF\ncg <tyu4778@gmail.com>
2023-12-08 13:32:01 +0900

    Create: DP 문제 pascal-triangle(Top-down)

--- study/codingTest_[All In One]/6. DP/My work/pascals-triangle(Top-down).py
+++ study/codingTest_[All In One]/6. DP/My work/pascals-triangle(Top-down).py
@@ -4,9 +4,21 @@
 class Solution(object):
     def generate(self, numRows):
         memo = [[1] * i for i in range(1, numRows + 1)]
-        
+        def dp(numRows):
+            # base case
+            if numRows == 1 or numRows == 2:
+                return memo[numRows - 1]
+            
+            # recursion
+            prev_row = dp(numRows - 1)
+
+            for i in range(1, numRows - 1):
+                memo[numRows - 1][i] = prev_row[i-1] + prev_row[i]
+
+            return memo[numRows - 1]
+        dp(numRows)
         return memo
 
-# sol = Solution()
-# answer = sol.generate(numRows = 5)
-# print(answer)
+sol = Solution()
+answer = sol.generate(numRows = 5)
+print(answer)
