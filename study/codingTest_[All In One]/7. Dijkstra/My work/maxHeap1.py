cd8e1f6d670eaa4cb3db96c498b6baa832e67fc2
DESKTOP-MDAH4MF\ncg <tyu4778@gmail.com>
2023-12-15 14:29:20 +0900

    🔥 remove: Delete 7.Dijkstra folder

--- study/codingTest_[All In One]/7. Dijkstra/My work/maxHeap1.py
+++
@@ -1,7 +0,0 @@
-import heapq
-
-max_heap = [5, 3, 9, 4, 1, 2, 6]
-heapq._heapify_max(max_heap) # [9, 4, 6, 3, 1, 2, 5]
-value = heapq._heappop_max(max_heap) # [6, 4, 5, 3, 1, 2]
-
-print(value) # 9
--- study/codingTest_[All In One]/7. Dijkstra/My work/minHeap.py
+++
@@ -1,9 +0,0 @@
-import heapq
-
-min_heap = [5, 3, 9, 4, 1, 2, 6]
-
-heapq.heapify(min_heap) # [1, 3, 2, 4, 5, 9, 6]
-heapq.heappop(min_heap) # [2, 3, 6, 4, 5, 9]
-heapq.heappush(min_heap, 1) # [1, 3, 2, 4, 5, 9, 6]
-
-print(min_heap) # [1, 3, 2, 4, 5, 9, 6]
