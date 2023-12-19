e043544ced1dc6223d9630f9df0f5ee515b681a3
DESKTOP-MDAH4MF\ncg <tyu4778@gmail.com>
2023-12-19 18:01:22 +0900

    💡 comment(합승 택시 요금): 시간복잡도 계산 추가

--- programmers/python/dijkstra/shared_taxi_fare.py
+++ programmers/python/dijkstra/shared_taxi_fare.py
@@ -1,8 +1,17 @@
 '''
+2021 KAKAO BLIND RECRUITMENT
 level 3
 dijkstra
 합승 택시 요금
 https://school.programmers.co.kr/learn/courses/30/lessons/72413
+'''
+
+'''
+3 <= n <= 200
+1 <= s, a, b <= n
+fares [[]]
+2 <= fares <= n(n-1) / 2 
+간선(Edge)의 최대 200*199 / 2 = 19,900 = (2 * 10^5)
 '''
 
 '''
@@ -20,12 +29,13 @@ def solution(n, s, a, b, fares):
     
     # 1. fares -> graph로 변경 (양방향)
     graph = defaultdict(list)
-    for n1, n2, cost in fares:
+    for n1, n2, cost in fares: # O(E) : O(19,900) = O(2 * 10^5)
         graph[n1].append((cost, n2)) # (cost, node)
         graph[n2].append((cost, n1))
     # return graph
         
-    # 2. dijkstra 알고리즘 사용
+    # 2. dijkstra 알고리즘 사용            # (2^10 = 1,024 / 2^14 = 16,384)
+    # O(E log E) = 20,000 * log 20,000 = 20,000 * log 2^15 =  20,000 * 15 = 300,000 = 3 * 10^5
     def dijkstra(start):
         costs = {}
         pq = []
@@ -44,7 +54,7 @@ def solution(n, s, a, b, fares):
     costsA = dijkstra(a)
     costsB = dijkstra(b)
     
-    # 3. 총 비용의 합 구하기 : O(V)
+    # 3. 총 비용의 합 구하기 : O(V) = O(200)
     # 초기 answer 값 : S에서 시작한 A와 B의 비용 합
     answer = costsS[a] + costsS[b]
 
